482e2aa8e4676eb0103766db34fe918deda233d4
Daryn Lam <dkl67@drexel.edu>
2025-12-04 17:41:58 -0500

    Add first batch of images

---
+++ src/routes/+page.server.js
@@ -0,0 +1,15 @@
+import {supabase} from '$lib/supabase'
+
+export async function load() {
+    const { data, error } = await supabase
+    .from('dkl67_idm364')
+    .select('*')
+
+    if (error) console.error('Error: ', error)
+    else console.log('Data: ', data)
+
+    return {
+        products: data
+    }
+}
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
+<script lang="js">
+    let {data} = $props()
+    $inspect(data)
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <h1>Hello and welcome to my site!</h1>
 <a href="/product">Buy an item</a>
+
+{#if data.products}
+    {#each data.products as product}
+        <p>{product.name}</p>
+    {/each}
+{/if}
