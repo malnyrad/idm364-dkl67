64b1da8cf7f2703f8aded4048c6813432deafe22
Daryn Lam <dkl67@drexel.edu>
2025-12-10 22:28:23 -0500

    Update database items

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,6 +4,7 @@ export async function load() {
     const { data, error } = await supabase
     .from('dkl67_idm364')
     .select('*')
+    .order("id", { ascending: true });
 
     if (error) console.error('Error: ', error)
     else console.log('Products: ', data)
