1de1c86adeca88d6b69609ea0b37fc51937cba7f
Daryn Lam <dkl67@drexel.edu>
2025-12-12 11:37:39 -0500

    Complete basic element chip

--- src/lib/components/ElementChip.svelte
+++ src/lib/components/ElementChip.svelte
@@ -2,16 +2,26 @@
     let { element } = $props();
 </script>
 
-<div class="element">
-    {#if element === "Earth"}
+{#if element === "Earth"}
+    <div class="element" style="background-color: #956127">
+        <img src="/images/earth.webp" alt="" />
         <p>Earth</p>
-    {:else if element === "Fire"}
+    </div>
+{:else if element === "Fire"}
+    <div class="element" style="background-color: #c71f1b;">
+        <img src="/images/fire.webp" alt="" />
         <p>Fire</p>
-    {:else if element === "Water"}
+    </div>
+{:else if element === "Water"}
+    <div class="element" style="background-color: #4e73eb;">
+        <img src="/images/water.webp" alt="" />
         <p>Water</p>
-    {:else if element === "Wind"}
+    </div>
+{:else if element === "Wind"}
+    <div class="element" style="background-color: #35a560">
+        <img src="/images/wind.webp" alt="" />
         <p>Wind</p>
-    {:else}
-        <p>Error</p>
-    {/if}
-</div>
+    </div>
+{:else}
+    <p>Error</p>
+{/if}
